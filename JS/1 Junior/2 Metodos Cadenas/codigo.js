67b91f5b83058bdf365eee7df1992aa404ccbd5f
andresvils <fenixblanco789@gmail.com>
2024-09-25 13:23:39 -0300

    add curso

---
+++ JS/1 Junior/2 Metodos Cadenas/codigo.js
@@ -0,0 +1,14 @@
+let cadena="cadena de prueba"
+
+resultado=cadena.concat(" cadena agregada")//concatena cadenas
+
+resultado_1=cadena.startsWith("a")//compara las primeras letra de una cadena con las primeras de otra
+
+resultado_2=cadena.endsWith("ba")//compara la ultimas con las ultimas de la otra cadena
+
+resultado_3=cadena.includes("j")//busca cadenas en otras cadenas
+
+document.write(resultado+`<br>`)
+document.write(resultado_1+`<br>`)
+document.write(resultado_2+`<br>`)
+document.write(resultado_3+`<br>`)
